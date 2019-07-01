bcedceb92ae9010952eca8faab635b060c56b58c
Ian Hollander <ianholla@gmail.com>
2019-07-01 14:25:34 -0400

    vma303 test code

---
+++ vma303_test.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import time
+import datetime
+
+# initialize GPIO
+GPIO.setwarnings(false)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+while True
+  print("Water sensor: ", GPIO.input(17))
+
+  time.sleep(1)
