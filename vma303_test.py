16f0f6cea6b72e951fbeeb7853d10729b24028e6
Ian Hollander <ianholla@gmail.com>
2019-07-01 14:30:33 -0400

    channel setup

--- vma303_test.py
+++ vma303_test.py
@@ -7,7 +7,11 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
+channel = 17
+
+GPIO.setup(channel, GPIO.IN)
+
 while True:
-  print("Water sensor: ", GPIO.input(17))
+  print("Water sensor: ", GPIO.input(channel))
 
   time.sleep(1)
